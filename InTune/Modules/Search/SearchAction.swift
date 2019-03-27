cc293b94d05f4234f1b09923d19c16ad5c4dd5bd
Ronan O Ciosoig <ronan.o.ciosoig@stanwood.de>
2019-03-27 11:58:19 +0100

    Add comments to give context.

--- InTune/Modules/Search/SearchAction.swift
+++ InTune/Modules/Search/SearchAction.swift
@@ -18,9 +18,14 @@ extension AppController: SearchActions {
     func search(term: String) {
         dataProvider.search(term: term)
         
+        // If therre is a delay in the simulator, then since the network responds faster,
+        // the call to show the loading HUD can happen after the response has been received, and thus
+        // it can get stuck on the screen.
         #if arch(i386) || arch(x86_64)
         self.coordinator?.showLoading()
         #else
+        // This fixes a UI bug when the loading HUD appears while the keyboard is still being displayed
+        // By delaying the call to show the loading HUD, it gives time for the keyboard to hide.
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
             self.coordinator?.showLoading()
         }
