e0ed0af029a84a60aaba07c998f7c852f44ae808
Ronan O Ciosoig <ronan.o.ciosoig@stanwood.de>
2019-03-27 11:58:34 +0100

    Code cleanup

--- InTune/Modules/Song Detail/SongDetailDataProvider.swift
+++ InTune/Modules/Song Detail/SongDetailDataProvider.swift
@@ -9,30 +9,10 @@
 import Foundation
 
 protocol SongDetailDataProvider {
-    func nextResult() -> Result?
-    func previousResult() -> Result?
     func selectedResult() -> Result?
 }
 
 extension DataProvider: SongDetailDataProvider {
-    func nextResult() -> Result? {
-        if appData.selectedIndex < appData.searchResults.count - 1 {
-            appData.selectedIndex += 1
-        } else {
-            appData.selectedIndex = 0
-        }
-        return selectedResult()
-    }
-    
-    func previousResult() -> Result? {
-        if appData.selectedIndex > 0 {
-           appData.selectedIndex -= 1
-        } else {
-            appData.selectedIndex = appData.searchResults.count - 1
-        }
-        return selectedResult()
-    }
-    
     func selectedResult() -> Result? {
         let selectedIndex = appData.selectedIndex
         return result(at: selectedIndex)
