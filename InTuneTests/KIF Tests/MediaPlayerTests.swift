071a63b566b8d6cd7fa857596ff70deb3bce5ffb
Ronan O Ciosoig <ronan.o.ciosoig@stanwood.de>
2019-03-28 19:28:51 +0100

    Refactor media player tests into own test class.

---
+++ InTuneTests/KIF Tests/MediaPlayerTests.swift
@@ -0,0 +1,64 @@
+//
+//  MediaPlayerTests.swift
+//  InTuneTests
+//
+//  Created by Ronan on 28/03/2019.
+//  Copyright © 2019 Sonomos. All rights reserved.
+//
+
+import XCTest
+
+class MediaPlayerTests: KIFTestCase {
+    
+    let searchText = "Moby"
+    
+    override func setUp() {
+        
+    }
+    
+    func testSearchAndPlayInDetail() {
+        tester().enterText(searchText, intoViewWithAccessibilityLabel: Constants.Accessibility.SearchScreen.SearchBar.searchTextField)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.searchButton)
+        tester().waitForAnimationsToFinish()
+        tester().waitForView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.sortButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.cell)
+        tester().waitForAnimationsToFinish()
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.playButton)
+    }
+    
+    func testSearchAndPlayNextInDetail() {
+        tester().enterText(searchText, intoViewWithAccessibilityLabel: Constants.Accessibility.SearchScreen.SearchBar.searchTextField)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.searchButton)
+        tester().waitForAnimationsToFinish()
+        tester().waitForView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.sortButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.cell)
+        tester().waitForAnimationsToFinish()
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.playButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.nextButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.playButton)
+    }
+    
+    func testSearchAndPlayAndBack() {
+        tester().enterText(searchText, intoViewWithAccessibilityLabel: Constants.Accessibility.SearchScreen.SearchBar.searchTextField)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.searchButton)
+        tester().waitForAnimationsToFinish()
+        tester().waitForView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.sortButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.cell)
+        tester().waitForAnimationsToFinish()
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.playButton)
+        tester().tapView(withAccessibilityLabel: "Results")
+    }
+    
+    func testSearchAndPlayAndBackAndPlay() {
+        tester().enterText(searchText, intoViewWithAccessibilityLabel: Constants.Accessibility.SearchScreen.SearchBar.searchTextField)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.searchButton)
+        tester().waitForAnimationsToFinish()
+        tester().waitForView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.sortButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.SearchScreen.cell)
+        tester().waitForAnimationsToFinish()
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.playButton)
+        tester().tapView(withAccessibilityLabel: "Results")
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.playButton)
+        tester().tapView(withAccessibilityLabel: Constants.Accessibility.MediaPlayer.nextButton)
+    }
+}
