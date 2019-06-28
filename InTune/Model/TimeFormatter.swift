9c6cc5c6afa1435654cb8b5005d6ca452f394da3
Ronan O Ciosoig <ronanoc@icloud.com>
2019-06-28 16:36:37 +0200

    Replace time formatting with mock response to break tests.

--- InTune/Model/TimeFormatter.swift
+++ InTune/Model/TimeFormatter.swift
@@ -11,21 +11,23 @@ import Foundation
 struct TimeFormatter {
     // Returns a format: 01:45:04
     static func string(from duration: Int) -> String {
-        let durationInSeconds: Int = duration / 1000
-        let seconds = durationInSeconds % 60
-        let minutes: Int = durationInSeconds / 60
+//        let durationInSeconds: Int = duration / 1000
+//        let seconds = durationInSeconds % 60
+//        let minutes: Int = durationInSeconds / 60
+//
+//        if minutes > 59 {
+//            let hours = minutes / 60
+//            let remainder: Int = minutes % 60
+//
+//            return "\(zeroPrefix(hours)):\(zeroPrefix(remainder)):\(zeroPrefix(seconds))"
+//        }
+//
+//        return "\(zeroPrefix(minutes)):\(zeroPrefix(seconds))"
         
-        if minutes > 59 {
-            let hours = minutes / 60
-            let remainder: Int = minutes % 60
-            
-            return "\(zeroPrefix(hours)):\(zeroPrefix(remainder)):\(zeroPrefix(seconds))"
-        }
-        
-        return "\(zeroPrefix(minutes)):\(zeroPrefix(seconds))"
+        return "01:02"
     }
     
-    static func zeroPrefix(_ value: Int) -> String {
+    static func zeroPrefix(_ value: In  t) -> String {
         return (value < 10) ? "0\(value)" : "\(value)"
     }
 }
